df0d2483e79718feb50496c0480f39105efe8318
Nicholas Hickman <nhickman@dtechlabs.com>
2010-10-13 18:49:13 -0400

    spaces missing after formating
    Signed-off-by: Nicholas Hickman <nhickman@dtechlabs.com>

--- pppd/md4.h
+++ pppd/md4.h
@@ -27,7 +27,7 @@ typedef struct {
  ** Initialize the MD4_CTX prepatory to doing a message digest
  ** computation.
  */
-extern void MD4Init__P((MD4_CTX *MD));
+extern void MD4Init __P((MD4_CTX *MD));
 
 /* MD4Update(MD,X,count)
  ** Input: X -- a pointer to an array of unsigned characters.
@@ -41,21 +41,21 @@ extern void MD4Init__P((MD4_CTX *MD));
  ** every MD computation should end with one call to MD4Update with a
  ** count less than 512.  Zero is OK for a count.
  */
-extern void MD4Update__P((MD4_CTX *MD, unsigned char *X, unsigned int count));
-
+extern void MD4Update __P((MD4_CTX *MD, unsigned char *X, unsigned int count));
+ 
 /* MD4Print(MD)
  ** Prints message digest buffer MD as 32 hexadecimal digits.
  ** Order is from low-order byte of buffer[0] to high-order byte
  ** of buffer[3].
  ** Each byte is printed with high-order hexadecimal digit first.
  */
-extern void MD4Print__P((MD4_CTX *));
+extern void MD4Print __P((MD4_CTX *));
 
 /* MD4Final(buf, MD)
  ** Returns message digest from MD and terminates the message
  ** digest computation.
  */
-extern void MD4Final__P((unsigned char *, MD4_CTX *));
+extern void MD4Final __P((unsigned char *, MD4_CTX *));
 
 /*
  ** End of md4.h
